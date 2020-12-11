145802601264a20cc0466607c9ff723bbc07db13
tomer0702 <utkarsh.tomer@successive.tech>
2020-12-11 18:42:43 +0530

    add :React Introduction to state  feature/40431

--- training/src/pages/TextFieldDemo/TextFieldDemo.jsx
+++ training/src/pages/TextFieldDemo/TextFieldDemo.jsx
@@ -2,13 +2,20 @@ import React from 'react';
 
 import { TextField } from '../../components';
 
+import { Slider } from '../../components/Slider';
+
+import { banners } from '../../config/constants';
+
 import { Div } from '../../components/TextField/style';
 
 const TextFieldDemo = () => (
   <Div>
+    <div>
+      <Slider alt="No Image" duration="1000" height="300" random banner={banners} />
+    </div>
     <p><b>This is a Disabled Input</b></p>
     <TextField disabled value="disabled input" />
-    <p><b> A Valid Inpu</b></p>
+    <p><b> A Valid Input</b></p>
     <TextField value="accessible" />
     <p><b>An input with Errors </b></p>
     <TextField error="Could not be more than" value="101" />
