519ad328926cc7f7345fbd7d4e1459f92d98f96a
tomer0702 <utkarsh.tomer@successive.tech>
2020-12-24 20:18:00 +0530

    modify :changes  feature/40791

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 // import { render } from '@testing-library/react';
 import React from 'react';
+import { ThemeProvider } from '@material-ui/core';
 import { ChildrenDemo } from './pages/ChildrenDemo';
+import Theme from './theme';
 
 const App = () => (
-  <ChildrenDemo />
+  <ThemeProvider theme={Theme}>
+    <ChildrenDemo />
+  </ThemeProvider>
 );
 
 export default App;
--- src/components/Math/Math.jsx
+++ src/components/Math/Math.jsx
@@ -1,50 +1,41 @@
+/* eslint-disable no-eval */
 import React from 'react';
 import PropTypes from 'prop-types';
+
+const getResult = (first, second, operator) => {
+  let result;
+  if (!(operator === '+' || operator === '-' || operator === '*' || operator === '/')) {
+    result = 'invalid operator';
+  } else {
+    result = eval(`${first} ${operator} ${second}`);
+  }
+  return result;
+};
 
 const Calculation = (props) => {
   const {
     first, second, operator, children,
   } = props;
-  let { result } = props;
-  switch (operator) {
-  case '+': result = first + second;
-    break;
-  case '-': result = first - second;
-    break;
-  case '*': result = first * second;
-    break;
-  case '/': result = first / second;
-    break;
-  default: return ('invalid operator');
-  }
   if (children) {
-    return children(first, second, result);
+    return children(first, second, getResult(first, second, operator), operator);
   }
   return (
-    <>
-      <p>
-        {' '}
-        {first}
-        {' '}
-        {operator}
-        {' '}
-        {second}
-        {' '}
-        =
-        {' '}
-        {result}
-        {' '}
-      </p>
-    </>
+    <p>
+      {`Result of ${first} ${operator} ${second} = `}
+      {
+        getResult(first, second, operator)
+      }
+    </p>
   );
 };
+
 Calculation.propTypes = {
   first: PropTypes.number.isRequired,
   second: PropTypes.number.isRequired,
   operator: PropTypes.string.isRequired,
-  result: PropTypes.number.isRequired,
   children: PropTypes.func,
 };
+
 Calculation.defaultProps = {
   children: null,
 };
