a9cf2ceb9e915011293f822b1016db34f52e10b7
tomer0702 <utkarsh.tomer@successive.tech>
2020-12-28 18:30:59 +0530

    modify :changes  feature/41046

--- src/pages/ChildrenDemo/ChildrenDemo.jsx
+++ src/pages/ChildrenDemo/ChildrenDemo.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import { ThemeProvider } from '@material-ui/core';
 import Calculation from '../../components/Math/Math';
-import Theme from '../../theme';
 
 export default class CalcultorDemo extends React.Component {
   Result() {
@@ -13,53 +11,51 @@ export default class CalcultorDemo extends React.Component {
   render() {
     return (
       <>
-        <ThemeProvider theme={Theme}>
-          <Calculation first={7} second={4} operator="+" />
-          <Calculation first={7} second={3} operator="-" />
-          <Calculation first={7} second={20} operator="*" />
-          <Calculation first={7} second={0} operator="/" />
-          <Calculation first={7} second={4} operator="+">
-            {
-              (first, second, result) => (
-                <p>
-                  Sum of
-                  {' '}
-                  {first}
-                  {' '}
-                  and
-                  {' '}
-                  {second}
-                  {' '}
-                  is equal to
-                  {' '}
-                  {result}
-                  {' '}
-                </p>
-              )
-            }
-          </Calculation>
-          <Calculation first={3} second={4} operator="+">
-            {
-              (first, second, result) => (
-                <p>
-                  When we add
-                  {' '}
-                  {first}
-                  {' '}
-                  with
-                  {' '}
-                  {second}
-                  {' '}
-                  then we will get
-                  {' '}
-                  {result}
-                  {' '}
-                  as result
-                </p>
-              )
-            }
-          </Calculation>
-        </ThemeProvider>
+        <Calculation first={7} second={4} operator="+" />
+        <Calculation first={7} second={3} operator="-" />
+        <Calculation first={7} second={20} operator="*" />
+        <Calculation first={7} second={0} operator="/" />
+        <Calculation first={7} second={4} operator="+">
+          {
+            (first, second, result) => (
+              <p>
+                Sum of
+                {' '}
+                {first}
+                {' '}
+                and
+                {' '}
+                {second}
+                {' '}
+                is equal to
+                {' '}
+                {result}
+                {' '}
+              </p>
+            )
+          }
+        </Calculation>
+        <Calculation first={3} second={4} operator="+">
+          {
+            (first, second, result) => (
+              <p>
+                When we add
+                {' '}
+                {first}
+                {' '}
+                with
+                {' '}
+                {second}
+                {' '}
+                then we will get
+                {' '}
+                {result}
+                {' '}
+                as result
+              </p>
+            )
+          }
+        </Calculation>
       </>
     );
   }
